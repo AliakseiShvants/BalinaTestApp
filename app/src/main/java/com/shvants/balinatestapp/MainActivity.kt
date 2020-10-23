36d54854495f5f12a913ad5df4f4aba9ce3f35c1
Aliaksei_Shvants <Aliaksei_Shvants@epam.com>
2020-10-23 12:39:05 +0300

    done with splash screen

--- app/src/main/java/com/shvants/balinatestapp/MainActivity.kt
+++ app/src/main/java/com/shvants/balinatestapp/MainActivity.kt
@@ -1,11 +1,6 @@
 package com.shvants.balinatestapp
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 }
